0cabed7348ac377e6e6d98482ddac9d85ccb55d1
kotli <myepicmail@gmail.com>
2020-08-01 09:38:32 +0300

    add rate limit speed limit and app api key

--- src/api/ashdod-weather.js
+++ src/api/ashdod-weather.js
@@ -1,24 +1,52 @@
 const express = require('express');
 const axios = require('axios');
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
 
 const router = express.Router();
+const limiter = rateLimit({
+  windowMs: 30 * 1000, // 30 seconds
+  max: 10
+});
 
-const BASE_URL = 'api.openweathermap.org/data/2.5/weather?';
+const speedLimiter = slowDown({
+  windowMs: 30 * 1000, // 30 seconds
+  delayAfter: 1,
+  delayMs: 500
+});
 
-router.get('/', async (req, res, next) => {
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
+let cacheData;
+let cacheTime;
+
+router.get('/', limiter, speedLimiter, (req, res, next) => {
+  //middleware
+  const apiKeys = req.get(`X-API-KEY`);
+  if (process.env.API_KEY === apiKeys) {
+    next();
+  } else {
+    const error = new Error(`Invalid API Key`);
+    next(error);
+  }
+}, async (req, res, next) => {
+  // in memory cache
+  if (cacheTime && cacheTime > Date.now() > -30 * 1000) {
+    return res.json(cacheData);
+  }
   try {
     const params = new URLSearchParams({
       appid: process.env.OPENWEATHERMAP_API_KEY,
       q: 'ashdod',
     });
-    const data = await axios.get(`${BASE_URL}${params}`);
-    res.json(data);
+    const { data } = await axios.get(`${BASE_URL}${params}`);
+    cacheData = data;
+    cacheTime = Date.now();
+    data.cacheTime = cacheTime;
+    return res.json(data);
   } catch (error) {
     // will call my error handler
-    console.log('>>>>>>>>>>>>>>>>>>>>>>');
-    console.log(error);
-    console.log('<<<<<<<<<<<<<<<<<<<<<<');
-    next(error);
+    return next(error);
   }
 });
 
